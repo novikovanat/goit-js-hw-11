a71bdd8b83a114fba33c8ede63c1503985e1edc8
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-03-08 22:54:18 +0100

    notification added

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,10 @@
+import {renderGallery, addStartMarkup} from "./render-functions";
+import iziToast from "izitoast";
+import errorSvg from '../img/error.svg';
+
 //  functions for HTTP requests.
- import {renderGallery, addStartMarkup} from "./render-functions";
-export default function fetchPhotos(search,loader){
+
+export default function fetchPhotos(search){
 const searchParams = new URLSearchParams({
     key :"42747257-6cb1908b03b224c2fc7af7612",
     q: `"${search}"`,
@@ -10,7 +14,6 @@ const searchParams = new URLSearchParams({
   });
 
 
-console.log(`"https://pixabay.com/api/?${searchParams}"`)
 fetch(`https://pixabay.com/api/?${searchParams}`)
   .then((response) => {
     if (!response.ok) {
@@ -19,7 +22,7 @@ fetch(`https://pixabay.com/api/?${searchParams}`)
     let photosObject;
     return photosObject = response.json()
   })
-  .then((photosObject) => { console.log(photosObject)
+  .then((photosObject) => { 
     const {total, hits} = photosObject
   
     if (total>0){
@@ -27,7 +30,22 @@ fetch(`https://pixabay.com/api/?${searchParams}`)
     } else
     {
       addStartMarkup()
-      console.log("Sorry, there are no images matching your search query. Please try again!")
+      (iziToast.error({
+        timeout:'5000',
+        messageColor:'#ffffff',
+        titleColor:"#fff",
+        titleSize:"16",
+        titleLineHeight:'24',
+        message: "Sorry, there are no images matching your search query. Please try again!",
+        iconUrl: errorSvg,
+        iconColor:'#fff',
+        backgroundColor:'#EF4040',
+        progressBarColor:"#B51B1B",
+        position:'topRight',
+        messageSize:'16',
+        messageLineHeight:'24',
+        maxWidth:'432px'
+    }));
       }
 
     
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import fetchPhotos from'./js/pixabay-api';
+import errorSvg from './img/error.svg'
+import iziToast from 'izitoast';
 
 
 const form = document.querySelector('form')
 const loader = document.querySelector('.loader-css')
-// const gallery = document.querySelector('.gallery');
+
 
 form.addEventListener("input", inputHandler)
 form.addEventListener("submit", submitHandler)
@@ -13,14 +15,29 @@ form.addEventListener("submit", submitHandler)
 let searchPrase;
 function inputHandler(){
     searchPrase = event.currentTarget.search.value;  
-    // console.log(" in input: ", searchPrase)  
 }
 
 function submitHandler(){
     event.preventDefault()
 
     if (searchPrase.trim() === '')
-    {
+    {(iziToast.error({
+        timeout:'5000',
+        messageColor:'#ffffff',
+        title:'Error',
+        titleColor:"#fff",
+        titleSize:"16",
+        titleLineHeight:'24',
+        message: "Empty input",
+        iconUrl: errorSvg,
+        iconColor:'#fff',
+        backgroundColor:'#EF4040',
+        progressBarColor:"#B51B1B",
+        position:'topRight',
+        messageSize:'16',
+        messageLineHeight:'24',
+        maxWidth:'432px'
+    }));
         console.log("Empty input")}
     else {
     
